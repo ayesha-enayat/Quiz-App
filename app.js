2fb372188d3dd0932ee5809aa5b93279b527d8cf
ayeshaenayat <ayeshaenayat123enayat@gmail.com>
2024-09-09 00:15:32 +0500

    create questions array

---
+++ app.js
@@ -0,0 +1,50 @@
+var questions = [
+    {
+        question: "What does HTML stand for?",
+        opt1: "Hyper Tag Markup Language",
+        opt2: "Hyper Text Markup Language",
+        opt3: "Hyperlinks Text Mark Language",
+        opt4: "Hyperlinking Text Marking Language",
+        ans: "Hyper Text Markup Language"
+    },
+    {
+        question: " Which method is used to parse a string to an integer in JavaScript?",
+        opt1: "parseInt()",
+        opt2: "parseInteger()",
+        opt3: "parseNumber()",
+        opt4: "parseFloat()",
+        ans: "parseInt()"
+
+    },
+    {
+        question: "Which of these is a genuine tag keyword?",
+        opt1: "Header",
+        opt2: "Bold",
+        opt3: "Body",
+        opt4: "Image",
+        ans: "Body"
+    },
+    {
+        question: "What symbol indicates a tag?",
+        opt1: "Angle brackets e.g <>",
+        opt2: "Curved brackets e.g. {,}",
+        opt3: "Commas e.g. ','",
+        opt4: "Exclamation marks e.g. !",
+        ans: "Angle brackets e.g <>"
+
+    },
+    {
+        question: "What is the correct format for aligning written content to the center of the page in CSS?",
+        opt1: "Text-align:center;",
+        opt2: "Font-align:center;",
+        opt3: "Text:align-center",
+        opt4: "align-center;",
+        ans: "Text-align:center;"
+    },
+
+];
+
+var container=document.getElementById('container');
+container.innerHTML +=``
+
+;
